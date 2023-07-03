cc797e284cc8497e4b25748e5bea4004ca6d0fe7
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-07-03 17:24:53 +0530

    neccessary css is completed and python add in a docker file

--- reacts/src/App.js
+++ reacts/src/App.js
@@ -1,73 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [inputText, setInputText] = useState('');
-  const [ngrams, setNgrams] = useState('');
-
+  const [inputText, setInputText] = useState("");
+  const [ngrams, setNgrams] = useState("");
+  const [backgroundColor, setBackgroundColor] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+
     // Make a POST request to your backend to insert the text data
     axios
-      .post('http://localhost:5000/text-data', { text: inputText })
+      .post("http://localhost:5000/text-data", { text: inputText })
       .then((response) => {
         // Handle successful response from the backend
         console.log(response.data);
         // Update the UI or perform any other actions based on the response
-  
+
         // Call the function to retrieve ngrams here
         getNgrams();
+        setBackgroundColor("#FFB6C1");
+
+        setTimeout(() => {
+          setBackgroundColor("");
+        }, 5000);
       })
       .catch((error) => {
         // Handle error
-        if (error.response && error.response.data && error.response.data.error) {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error
+        ) {
           console.error(error.response.data.error);
         } else {
           console.error(error);
         }
       });
   };
-  
+
   // Function to call Django API and retrieve ngrams
   async function getNgrams() {
     try {
-      const response = await axios.get('http://localhost:5000/ngrams');
-      setNgrams(response.data.text)
-      console.log('Ngrams Comparison:', response.data.text);
+      const response = await axios.get("http://localhost:5000/ngrams");
+      setNgrams(response.data.text);
+      console.log("Ngrams Comparison:", response.data.text);
       // Process the ngrams data or update the UI based on the comparison result
     } catch (error) {
-      console.error('Error retrieving ngrams:', error);
+      console.error("Error retrieving ngrams:", error);
     }
   }
-  
-
 
   return (
-    <div>
-      <h1>Text Input Form</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+    <div >
+      <div className="app-container" style={{ backgroundColor }}>
+        <h1 className="app-title">Text Input Form</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="input-text"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
 
-      {/* Display the ngramsArray in array format */}
-      <h2>Ngrams Comparison:</h2>
-      {ngrams.length > 0 ? (
-        <ul>
-          {ngrams.map((ngram, index) => (
-            <li key={index}>{ngram.join(' ')}</li>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
 
-          ))}
-        </ul>
-      ) : (
-        <p>No ngrams to display</p>
-      )}
+        {/* Display the ngramsArray in array format */}
+        <div className="ngrams-container">
+          <h2 className="ngrams-title">Ngrams Comparison:</h2>
+          {ngrams.length > 0 ? (
+            <ul className="ngrams-list">
+              {ngrams.map((ngram, index) => (
+                <li key={index} className="ngrams-item">
+                  {ngram.join(" ")}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="no-ngrams">No ngrams to display</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
